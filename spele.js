af565b0cacf7339f1a08e804124ab312d98dc159
andriszeile <71541542+andriszeile@users.noreply.github.com>
2026-01-16 09:09:47 +0200

    16_01_26

--- spele.js
+++ spele.js
@@ -1,3 +1,23 @@
 //no URL iegūst vārdu
 let adrese = window.location.hash.substring(1);
-adrese = decodeURI(adrese.split(',')[0]);
+let vards = decodeURI(adrese.split(',')[0] || '').trim();
+
+//mainīgie spēles darbībai
+let laiks = 0; //sekundes
+let klikski = 0;
+
+//taimera mainīgie (taimeris strādās ar pirmo klikski)
+let timerId = null;
+let timerStarted = false;
+
+
+
+
+
+
+const laukumiSaturs = ['😙','🥸','👽','😺','😙','🥶','😇','🥶','🥸','😺','👽','😇'];
+let atvertiLaukumi = [];
+let pedejieDivi = [];
+
+//sajauc emoji nejaušā secībā
+let laukumiSajaukti = [...laukumiSaturs].sort(() => Math.random() - 0.5);
