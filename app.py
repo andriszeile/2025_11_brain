fc9df72e777de72a618b012ed03394b0f1aaa247
andriszeile <71541542+andriszeile@users.noreply.github.com>
2026-02-06 10:04:15 +0200

    111

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ import json
 
 base_dir = Path(__file__).resolve().parent
 app = Flask(__name__, template_folder=str(base_dir / 'templates'), static_folder=str(base_dir / 'static'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/spele')
+def spele():
+    return render_template('game.html')
+
+@app.route('/tops')
+def tops():
+    return render_template('top.html')
+
+@app.route('/par')
+def par():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
